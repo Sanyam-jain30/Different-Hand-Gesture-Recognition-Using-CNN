d0cabb1b2a2c299f11b528d93069e2b1ac3b3618
Sanyam-jain30 <78720027+Sanyam-jain30@users.noreply.github.com>
2022-04-25 16:40:09 +0530

    Add files via upload

--- create_gesture_data.py
+++ create_gesture_data.py
@@ -1,10 +1,9 @@
 import os
-
 import cv2
-import tensorflow as tf
-import numpy as np
-from keras.preprocessing.image import ImageDataGenerator
 import glob
+import json
+import numpy as np
+import tensorflow as tf
 
 background = None
 accumulated_weight = 0.5
@@ -14,14 +13,138 @@ ROI_bottom = 300
 ROI_right = 150
 ROI_left = 350
 
-path = "E:\PyCharm\PycharmProjects\SC-Project\Different-Hand-Gesture-Recognition-Using-CNN\Traindata"
+file = open('checkpoints.json')
+data = json.load(file)
+
+test_path = data['paths'][0]['path_test_data']
+path = data['paths'][0]['path_train_data']
 
 traindata = glob.glob(path + "\*")
 noOfGestures = len(traindata)
 print("Number of Gestures already available: ", noOfGestures)
 
-path = path + "\Gesture_" + str(noOfGestures)
+c_frame=-1
+p_frame=-1
 
+#Setting threshold for number of frames to compare
+thresholdframes=50
+
+
+## Let us restore the saved model
+sess = tf.Session()
+# Step-1: Recreate the network graph. At this step only graph is created.
+saver = tf.train.import_meta_graph('C:/Users/Raj Shah/Downloads/AHD_Project/handgest_1.meta')
+# Step-2: Now let's load the weights saved using the restore method.
+saver.restore(sess, tf.train.latest_checkpoint('./'))
+
+# Accessing the default graph which we have restored
+graph = tf.get_default_graph()
+
+# Now, let's get hold of the op that we can be processed to get the output.
+# In the original network y_pred is the tensor that is the prediction of the network
+y_pred = graph.get_tensor_by_name("y_pred:0")
+
+## Let's feed the images to the input placeholders
+x= graph.get_tensor_by_name("x:0")
+y_true = graph.get_tensor_by_name("y_true:0")
+y_test_images = np.zeros((1, 10))
+y_test_images = np.zeros((1, 10))
+
+# Real Time prediction
+def predict(frame, y_test_images):
+    image_size = 50
+    num_channels = 3
+    images = []
+    image = frame
+    cv2.imshow('test', image)
+    # Resizing the image to our desired size and preprocessing will be done exactly as done during training
+    image = cv2.resize(image, (image_size, image_size), 0, 0, cv2.INTER_LINEAR)
+    images.append(image)
+    images = np.array(images, dtype=np.uint8)
+    images = images.astype('float32')
+    images = np.multiply(images, 1.0 / 255.0)
+
+    # The input to the network is of shape [None image_size image_size num_channels]. Hence we reshape.
+    x_batch = images.reshape(1, image_size, image_size, num_channels)
+
+    ### Creating the feed_dict that is required to be fed to calculate y_pred
+    feed_dict_testing = {x: x_batch, y_true: y_test_images}
+    result = sess.run(y_pred, feed_dict=feed_dict_testing)
+    # result is of this format [probabiliy_of gest0,......,probability_of_gest9]
+    return np.array(result)
+
+
+# Open Camera object
+cap = cv2.VideoCapture(0)
+
+# Decrease frame size (4=width,5=height)
+cap.set(4, 700)
+cap.set(5, 400)
+
+h, s, v = 150, 150, 150
+i = 0
+while (i < 1000000):
+    ret, frame = cap.read()
+
+    cv2.rectangle(frame, (300, 300), (100, 100), (0, 255, 0), 0)
+    crop_frame = frame[100:300, 100:300]
+    # Blur the image
+    # blur = cv2.blur(crop_frame,(3,3))
+    blur = cv2.GaussianBlur(crop_frame, (3, 3), 0)
+
+    # Convert to HSV color space
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+    # Create a binary image with where white will be skin colors and rest is black
+    mask2 = cv2.inRange(hsv, np.array([2, 50, 50]), np.array([15, 255, 255]))
+    med = cv2.medianBlur(mask2, 5)
+
+    ##Displaying frames
+    cv2.imshow('main', frame)
+    cv2.imshow('masked', med)
+
+    ##resizing the image
+    med = cv2.resize(med, (50, 50))
+    ##Making it 3 channel
+    med = np.stack((med,) * 3)
+    ##adjusting rows,columns as per x
+    med = np.rollaxis(med, axis=1, start=0)
+    med = np.rollaxis(med, axis=2, start=0)
+    ##Rotating and flipping correctly as per training image
+    M = cv2.getRotationMatrix2D((25, 25), 270, 1)
+    med = cv2.warpAffine(med, M, (50, 50))
+    med = np.fliplr(med)
+    ##converting expo to float
+    np.set_printoptions(formatter={'float_kind': '{:f}'.format})
+    ##printing index of max prob value
+    ans = predict(med, y_test_images)
+    # print(ans)
+    # print(np.argmax(max(ans)))
+
+    # Comparing for 50 continuous frames
+    c_frame = np.argmax(max(ans))
+    if (c_frame == p_frame):
+        counter = counter + 1
+        p_frame = c_frame
+        if (counter == thresholdframes):
+            print(ans)
+            print("Gesture:" + str(c_frame))
+            counter = 0
+            i = 0
+    else:
+        p_frame = c_frame
+        counter = 0
+
+    # close the output video by pressing 'ESC'
+    k = cv2.waitKey(2) & 0xFF
+    if k == 27:
+        break
+    i = i + 1
+
+cap.release()
+cv2.destroyAllWindows()
+
+"""
 def cal_accum_avg(frame, accumulated_weight):
     global background
 
@@ -45,10 +168,8 @@ def segment_hand(frame, threshold=25):
     if len(contours) == 0:
         return None
     else:
-
         hand_segment_max_cont = max(contours, key=cv2.contourArea)
-
-        return (thresholded, hand_segment_max_cont)
+        return thresholded, hand_segment_max_cont
 
 
 cam = cv2.VideoCapture(0)
@@ -56,11 +177,24 @@ cam = cv2.VideoCapture(0)
 num_frames = 0
 element = 10
 num_imgs_taken = 0
+st = ''
+res = []
+
+def get_gesture_name(path):
+    n = input("Enter the Gesture name: ")
+    path = path + "\\" + n
+    if not os.path.exists(path):
+        os.mkdir(path)
+    if not os.path.exists(os.path.join(test_path, name)):
+        os.mkdir(os.path.join(test_path, name))
+    return n
+
+name = get_gesture_name(path)
 
 while True:
     ret, frame = cam.read()
 
-    # filpping the frame to prevent inverted image of captured frame...
+    # flipping the frame to prevent inverted image of captured frame...
     frame = cv2.flip(frame, 1)
 
     frame_copy = frame.copy()
@@ -70,6 +204,17 @@ while True:
     gray_frame = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (9, 9), 0)
 
+    # cv2.imshow("Sign Detection", frame_copy)
+    # k = cv2.waitKey(0)
+    # print(k, num_imgs_taken)
+
+    # while k != 27 and num_imgs_taken < 1:
+    #     k = cv2.waitKey(0)
+    #     st = st + chr(k)
+    #     cv2.putText(frame_copy, "Enter gesture name:" + st, (30, 50), cv2.FONT_ITALIC, 1, (255, 0, 0), 1)
+    #
+    # path = path + "\\" + st
+
     if num_frames < 60:
         cal_accum_avg(gray_frame, accumulated_weight)
         if num_frames <= 59:
@@ -78,7 +223,7 @@ while True:
             # cv2.imshow("Sign Detection",frame_copy)
 
     # Time to configure the hand specifically into the ROI...
-    elif num_frames <= 300:
+    elif num_frames <= 299:
 
         hand = segment_hand(gray_frame)
 
@@ -92,9 +237,6 @@ while True:
             # Draw contours around hand segment
             cv2.drawContours(frame_copy, [hand_segment + (ROI_right, ROI_top)], -1, (255, 0, 0), 1)
 
-            cv2.putText(frame_copy, str(num_frames) + "For" + str(element), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                        (0, 0, 255), 2)
-
             # Also display the thresholded image
             cv2.imshow("Thresholded Hand Image", thresholded)
 
@@ -105,8 +247,6 @@ while True:
 
         # Checking if we are able to detect the hand...
         if hand is not None:
-            if not os.path.exists(path):
-                os.mkdir(path)
 
             # unpack the thresholded img and the max_contour...
             thresholded, hand_segment = hand
@@ -116,14 +256,14 @@ while True:
 
             cv2.putText(frame_copy, str(num_frames), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
             # cv2.putText(frame_copy, str(num_frames)+"For" + str(element), (70, 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-            cv2.putText(frame_copy, str(num_imgs_taken) + 'images' + "For" + str(element), (200, 400),
+            cv2.putText(frame_copy, str(num_imgs_taken + 1) + 'images For' + str(element), (200, 400),
                         cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
             # Displaying the thresholded image
             cv2.imshow("Thresholded Hand Image", thresholded)
-            if num_imgs_taken <= 300:
-                # cv2.imwrite(r"D:\\gesture\\train\\"+str(element)+"\\" + str(num_imgs_taken+300) + '.jpg', thresholded)
-                cv2.imwrite(path + "\\" + "gest" + str(noOfGestures) + "_" + str(num_imgs_taken+1) + '.jpg', thresholded)
+            if num_imgs_taken <= 299:
+                cv2.imwrite(path + "\\" + name + "\\gest" + str(noOfGestures) + "_" + str(num_imgs_taken + 1) + '.jpg',
+                            thresholded)
             else:
                 break
             num_imgs_taken += 1
@@ -151,3 +291,4 @@ while True:
 
 cv2.destroyAllWindows()
 cam.release()
+"""
--- training.py
+++ training.py
@@ -1,40 +1,71 @@
+# Check the accuracy
+
+import os
+import math
+import matplotlib.image
 import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
-from keras.layers import Activation, Dense, Flatten, BatchNormalization, Conv2D, MaxPool2D, Dropout
+from keras.layers import Dense, Flatten, Conv2D, MaxPool2D
 from keras.optimizers import Adam, SGD
-from keras.metrics import categorical_crossentropy
 from keras.preprocessing.image import ImageDataGenerator
-import itertools
-import random
 import matplotlib.pyplot as plt
 import warnings
 import numpy as np
 import cv2
 from keras.callbacks import ReduceLROnPlateau
 from keras.callbacks import ModelCheckpoint, EarlyStopping
+import json
+
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+file = open("checkpoints.json")
+data = json.load(file)
 
-train_path = r'/Different-Hand-Gesture-Recognition-Using-CNN/Traindata'
-test_path = r'/Different-Hand-Gesture-Recognition-Using-CNN/Testdata'
+train_path = data['paths'][0]['path_train_data']
+test_path = data['paths'][0]['path_test_data']
 
-train_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path, target_size=(64,64), class_mode='categorical', batch_size=10,shuffle=True)
-test_batches = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path, target_size=(64,64), class_mode='categorical', batch_size=10, shuffle=True)
+train_batches = ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=train_path,
+                                                                                             target_size=(64, 64),
+                                                                                             class_mode='categorical',
+                                                                                             batch_size=10,
+                                                                                             shuffle=True)
+
+test_batches = ImageDataGenerator(
+    preprocessing_function=tf.keras.applications.vgg16.preprocess_input).flow_from_directory(directory=test_path,
+                                                                                             target_size=(64, 64),
+                                                                                             class_mode='categorical',
+                                                                                             batch_size=10,
+                                                                                             shuffle=True)
 
 imgs, labels = next(train_batches)
-labels = ['None', 'One finger', 'Two finger', 'Three finger', 'Four finger', 'Five finger', 'Right', 'Top', 'YOY', 'Awesome']
+labels = os.listdir(train_path)
 
 
-#Plotting the images...
+# Plotting the images...
 def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 10, figsize=(30,20))
-    axes = axes.flatten()
-    for img, ax in zip( images_arr, axes):
+    fig = plt.figure(figsize=(10, 7))
+    rows = math.ceil(len(labels) / 2)
+    cols = math.ceil(len(labels) / 5)
+
+    images = []
+    folders = os.listdir(train_path)
+    for i in folders:
+        files = os.listdir(os.path.join(train_path, i))
+        paths = [os.path.join(train_path, os.path.join(i, filename)) for filename in files]
+        images.append(max(paths, key=os.path.getctime))
+
+    for i in images:
+        img = matplotlib.image.imread(i)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        ax.imshow(img)
-        ax.axis('off')
-    plt.tight_layout()
+        fig.add_subplot(rows, cols, images.index(i) + 1)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(labels[images.index(i)])
+
+    plt.subplots_adjust(hspace=0.5, wspace=0)
+    plt.savefig("Result.png", bbox_inches='tight')
     plt.show()
 
 
@@ -42,51 +73,46 @@ plotImages(imgs)
 print(imgs.shape)
 print(labels)
 
+word_dict = {}
+
+for i in range(0, len(labels)):
+    word_dict[i] = labels[i]
+
 model = Sequential()
 
-model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64,64,3)))
+model.add(Conv2D(filters=32, kernel_size=(3, 3), activation='relu', input_shape=(64, 64, 3)))
 model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 
-model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding = 'same'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'))
 model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 
-model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding = 'valid'))
+model.add(Conv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='valid'))
 model.add(MaxPool2D(pool_size=(2, 2), strides=2))
 
 model.add(Flatten())
 
-model.add(Dense(64,activation ="relu"))
-model.add(Dense(128,activation ="relu"))
-#model.add(Dropout(0.2))
-model.add(Dense(128,activation ="relu"))
-#model.add(Dropout(0.3))
-model.add(Dense(10,activation ="softmax"))
-
-
-# In[23]:
-
+model.add(Dense(64, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(len(labels), activation="softmax"))
 
 model.compile(optimizer=Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0001)
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
 
-
-
 model.compile(optimizer=SGD(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=1, min_lr=0.0005)
 early_stop = EarlyStopping(monitor='val_loss', min_delta=0, patience=2, verbose=0, mode='auto')
 
+history2 = model.fit(train_batches, epochs=10, callbacks=[reduce_lr, early_stop],
+                     validation_data=test_batches)  # , checkpoint])
+imgs, labels = next(train_batches)  # For getting next batch of imgs...
 
-history2 = model.fit(train_batches, epochs=10, callbacks=[reduce_lr, early_stop],  validation_data = test_batches)#, checkpoint])
-imgs, labels = next(train_batches) # For getting next batch of imgs...
-
-imgs, labels = next(test_batches) # For getting next batch of imgs...
+imgs, labels = next(test_batches)  # For getting next batch of imgs...
 scores = model.evaluate(imgs, labels, verbose=0)
 print(model.metrics_names, scores)
-print(f'{model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+print(f'{model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1] * 100}%')
 
-
-#model.save('best_model_dataflair.h5')
 model.save('model.h5')
 
 print(history2.history)
@@ -96,15 +122,10 @@ imgs, labels = next(test_batches)
 model = keras.models.load_model(r"model.h5")
 
 scores = model.evaluate(imgs, labels, verbose=0)
-print(f'{model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1]*100}%')
+print(f'{model.metrics_names[0]} of {scores[0]}; {model.metrics_names[1]} of {scores[1] * 100}%')
 
-model.summary()
-
-scores #[loss, accuracy] on test data...
+scores  # [loss, accuracy] on test data...
 model.metrics_names
-
-
-word_dict = {0:'None',1:'One finger',2:'Two finger',3:'Three finger',4:'Four finger',5:'Five finger',6:'Right',7:'Top',8:'YoY',9:'Awesome'}
 
 predictions = model.predict(imgs, verbose=0)
 print("predictions on a small set of test data--")
@@ -112,7 +133,7 @@ print("")
 for ind, i in enumerate(predictions):
     print(word_dict[np.argmax(i)], end='   ')
 
-plotImages(imgs)
+# plotImages(imgs)
 print('Actual labels')
 for i in labels:
     print(word_dict[np.argmax(i)], end='   ')
